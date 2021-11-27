c34bdc36beabd3bf52277070318547e2cb99ae1e
Elena Vaseva <aglagla@yandex.ru>
2021-11-27 18:59:56 +0300

    Задачи

---
+++ Макет/javascript/java.js
@@ -0,0 +1,31 @@
+//1 задача
+var num = 3;
+alert(num);
+
+//2 задача
+var a = 10;
+var b = 2;
+alert(+a + b);
+alert(a-b);
+alert(a*b);
+alert(a/b);
+
+//3
+var c = 15;
+var d = 2;
+var result = +c + d;
+alert(result);
+
+//4
+var a = 10;
+var b = 2;
+var c = 5;
+alert(+a + b + c);
+
+//5
+var a = 17;
+var b = 10;
+var c = a - b;
+var d = 7;
+var result = +c + d;
+alert(result);
